cece31b741df268fb7bf9e6fd3157f0d93f1a7ec
Rémy <duval.remy.2021@gmail.com>
2020-09-25 18:30:09 +0200

    INV-4 refacto : add alpha animation to intro text

--- index.py
+++ index.py
@@ -8,7 +8,15 @@ pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 done = False
 
+# titre de la fenetre
+
 pygame.display.set_caption("Space Invaders")
+
+# creation de l'image de fond
+
+backgroundImage=pygame.image.load ("assets\start.jpg")
+
+# creation des textes
 
 titleFont = pygame.font.SysFont('Comic Sans MS', 40)
 
@@ -16,13 +24,28 @@ startTextFont = pygame.font.SysFont('Comic Sans MS', 30)
 
 title = titleFont.render('Space Invaders', False, (255, 255, 255))
 
-startText = startTextFont.render('Appuyer sur une touche pour jouer', False, (255, 255, 255))
+startText = startTextFont.render('to play', False, (255, 255, 255))
+
+startText.set_alpha(255)
+
+goToAlpha=True
 
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
 
+
+    if goToAlpha:
+        startText.set_alpha(startText.get_alpha()-1)
+        if startText.get_alpha() == 0:
+            goToAlpha=False
+    else:
+        startText.set_alpha(startText.get_alpha()+1)
+        if startText.get_alpha() == 255:
+            goToAlpha=True
+
+    screen.blit(backgroundImage, (-235, -200))
     screen.blit(title, (250, 0))
-    screen.blit(startText, (150, SCREEN_HEIGHT-100))
+    screen.blit(startText, (350, SCREEN_HEIGHT-100))
     pygame.display.flip()
