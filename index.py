73b71f62b7da02e46d1dc9f7a469cbdd17ab0f19
Rémy <duval.remy.2021@gmail.com>
2020-09-25 16:55:18 +0200

    INV-4 add window title / add text intro

--- index.py
+++ index.py
@@ -4,9 +4,15 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600))
 done = False
 
+pygame.display.set_caption("Space Invaders")
+
+myfont = pygame.font.SysFont('Comic Sans MS', 30)
+textsurface = myfont.render('Space Invaders, Appuyer sur une touche pour jouer', False, (255, 255, 255))
+
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
 
+    screen.blit(textsurface, (0, 0))
     pygame.display.flip()
