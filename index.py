d66c23ae5da61ed0809985625cc7cbddfb283606
Rémy <duval.remy.2021@gmail.com>
2020-09-25 17:23:52 +0200

    INV-4 refacto title and text intro

--- index.py
+++ index.py
@@ -1,18 +1,28 @@
 import pygame
 
+SCREEN_WIDTH=800
+
+SCREEN_HEIGHT=600
+
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 done = False
 
 pygame.display.set_caption("Space Invaders")
 
-myfont = pygame.font.SysFont('Comic Sans MS', 30)
-textsurface = myfont.render('Space Invaders, Appuyer sur une touche pour jouer', False, (255, 255, 255))
+titleFont = pygame.font.SysFont('Comic Sans MS', 40)
+
+startTextFont = pygame.font.SysFont('Comic Sans MS', 30)
+
+title = titleFont.render('Space Invaders', False, (255, 255, 255))
+
+startText = startTextFont.render('Appuyer sur une touche pour jouer', False, (255, 255, 255))
 
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
 
-    screen.blit(textsurface, (0, 0))
+    screen.blit(title, (250, 0))
+    screen.blit(startText, (150, SCREEN_HEIGHT-100))
     pygame.display.flip()
